9b5ad9abcb295ae5a835cd8ec187ba130f284835
Andrew Richards <richards_andrew2@columbusstate.edu>
2019-11-02 11:40:45 -0400

    registered new models
    new models added to admin site

--- ATMProject/ATMSite/ATM/admin.py
+++ ATMProject/ATMSite/ATM/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import AccountExtension, AtmCard, AtMachine, ATMachineRefill, Transaction
+from .models import AccountExtension, AtmCard, AtMachine, ATMachineRefill, Transaction, Phone_Change, Pin_Change, Cash_Withdrawal, Cash_Transfer, Balance_Enquiry
 
 # Register your models here.
 
@@ -8,4 +8,9 @@ admin.site.register(AccountExtension)
 admin.site.register(AtmCard)
 admin.site.register(AtMachine)
 admin.site.register(ATMachineRefill)
-#admin.site.register(Transaction)
+admin.site.register(Transaction)
+admin.site.register(Phone_Change)
+admin.site.register(Pin_Change)
+admin.site.register(Cash_Withdrawal)
+admin.site.register(Cash_Transfer)
+admin.site.register(Balance_Enquiry)
