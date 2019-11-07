4fb7f7eee654b5e97ac5340e9b1ce91b0e86bbe3
Andrew Richards <richards_andrew2@columbusstate.edu>
2019-11-07 09:42:38 -0500

    added pages and basic views
    added 'ATM/withdraw'
    added 'ATM/transfer'
    added 'ATM/enquiry'
    added 'ATM/confirm'
    
    and basic views

--- ATMProject/ATMSite/ATM/urls.py
+++ ATMProject/ATMSite/ATM/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('index/', views.index, name='index'),
+	path('home/', views.index, name='index'),
+	path('enquiry/', views.enquiry, name='enquiry'),
+	path('withdraw/', views.withdraw, name='withdraw'),
+	path('transfer/', views.transfer, name='transfer'),
+	path('confirm/', views.confirm, name='confirm'),
 ]
--- ATMProject/ATMSite/ATM/views.py
+++ ATMProject/ATMSite/ATM/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 
 def index(request):
 	return HttpResponse ("This is the index page of the ATM app.")
+
+def enquiry(request):
+	return HttpResponse("This is the balance enquiry page.")
+
+def withdraw(request):
+	return HttpResponse("This is the cash withdrawal page.")
+
+def transfer(request):
+	return HttpResponse("This is the cash transfer page.")
+
+def confirm(request):
+	return HttpResponse("This is the transaction confirmation page.")
