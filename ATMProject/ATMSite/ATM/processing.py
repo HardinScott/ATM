74242a65b91728241051c3d1965d0f86abef340c
Andrew Richards <richards_andrew2@columbusstate.edu>
2019-11-07 10:32:52 -0500

    completed ATM/confirm
    added processing.py and methods for each transaction type
    after POSTing a form that has a key 'TransactionType' in the POST dictionary with a valid value('Withdraw', 'Transfer', 'Enquiry'). The confirm view asks the appropriate method in processing to perform the transaction and return a message to display in the confirmation screen.

---
+++ ATMProject/ATMSite/ATM/processing.py
@@ -0,0 +1,14 @@
+def withdraw(request):
+	message = "Withdraw message"
+	#TODO
+	return message
+
+def transfer(request):
+	message = "Transfer message"
+	#TODO
+	return message
+
+def enquiry(request):
+	message = "Enquiry message"
+	#TODO
+	return message
--- ATMProject/ATMSite/ATM/views.py
+++ ATMProject/ATMSite/ATM/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .processing import *
 
 def index(request):
 	return HttpResponse ("This is the index page of the ATM app.")
@@ -14,4 +15,15 @@ def transfer(request):
 	return HttpResponse("This is the cash transfer page.")
 
 def confirm(request):
-	return HttpResponse("This is the transaction confirmation page.")
+
+	transType = request.POST['TransactionType']
+	if transType=="Withdraw":
+		message = processing.withdraw(request)
+	elif transType == "Transfer":
+		message = processing.transfer(request)
+	elif transType == "Enquiry":
+		message = processing.transfer(request)
+	else:
+		raise "Invalid transaction type"
+
+	return HttpResponse(message)
